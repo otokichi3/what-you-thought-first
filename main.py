6dead7d25bd44cfb67553718e1dc685dae9a5385
otokichi3 <kofunital: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@gmail.com>
2024-02-26 11:47:30 +0900

    merge: line -> main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import time
-from line_notify_bot import LINENotifyBot
+import requests
 from flask import Flask, make_response, request
 from flask_cors import CORS
 
@@ -9,6 +9,31 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+class LINENotifyBot:
+    API_URL = 'https://notify-api.line.me/api/notify'
+
+    def __init__(self, access_token):
+        self.__headers = {'Authorization': 'Bearer ' + access_token}
+
+    def send(
+            self, message,
+            image=None, sticker_package_id=None, sticker_id=None,
+    ):
+        payload = {
+            'message': message,
+            'stickerPackageId': sticker_package_id,
+            'stickerId': sticker_id,
+        }
+        files = {}
+        if image != None:
+            files = {'imageFile': open(image, 'rb')}
+        r = requests.post(
+            LINENotifyBot.API_URL,
+            headers=self.__headers,
+            data=payload,
+            files=files,
+        )
 
 @app.route('/', methods=['POST'])
 def send_thought(request=None):
